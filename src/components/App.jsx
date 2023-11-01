424e2ccd25aeb956aaee9cd00700486fe768feff
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2023-11-02 00:29:04 +0200

    structure site

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
+import { Routes, Route, Link } from 'react-router-dom';
+import css from './App.module.css';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.Wrapper}>
+      <div className={css.Container}>
+        <header className={css.Header}>
+          <h2>PRO MOVIE SEARCHER</h2>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/movies">Movies</Link>
+          </nav>
+        </header>
+        <main className={css.Main}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
     </div>
   );
 };
